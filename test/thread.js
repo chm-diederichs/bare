eeac7fc34e285cdf418361a79ff5ff830e1541e6
Christophe Diederichs <chm-diederichs@protonmail.com>
2024-02-26 12:57:41 +0000

    update thread test

--- test/thread.js
+++ test/thread.js
@@ -7,7 +7,13 @@ assert(Thread.isMainThread === true)
 
 const entry = path.join(__dirname, 'fixtures/thread.js')
 
-const thread = new Thread(entry, { data: Buffer.from('hello world') })
-thread.on('exit', () => console.log('thread has exited'))
+const detached = new Thread(entry, { data: Buffer.from('hello world') })
+detached.on('exit', () => console.log('thread has exited'))
 
-thread.join()
+setTimeout(() => {
+  const thread = new Thread(entry, { data: Buffer.from('hello world') })
+  thread.on('exit', () => console.log('thread has exited'))
+
+  thread.join()
+  console.log('all done')
+}, 200) // allow time for detached to exit
